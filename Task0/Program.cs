6ca0a6ec778f6960035f72ca6eb3da67171cd27e
Igor <k566yo@mail.ru>
2022-08-07 18:13:43 +0500

    Добавили квадратные числа

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.Write($"Квадрат числа {num} = {square}");
+
