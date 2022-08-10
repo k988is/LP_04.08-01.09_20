af91b4643dc7ef773e8ff42d9781a8a80bdafe0b
Igor <k566yo@mail.ru>
2022-08-10 18:26:05 +0500

    Добавили 11 задание

---
+++ Task11/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int result = (num / 100) * 10 + num % 10;
+Console.WriteLine($"{num} -> {result}");
