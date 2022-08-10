a0dcc53dee252690872bf9649906fac76c650e26
Igor <k566yo@mail.ru>
2022-08-10 18:30:46 +0500

    Добавили 16 задание

---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли одно число квадратом другого.
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a * a == b)
+{
+    Console.WriteLine("b квадрат a");
+    return;
+}
+if (Math.Pow(b, 2) == a)
+{
+    Console.WriteLine("a квадрат b");
+}
+else
+{
+    Console.WriteLine("не является");
+}
